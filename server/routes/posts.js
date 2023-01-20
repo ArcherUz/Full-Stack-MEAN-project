2d34101f0069ccf24044f2157bf67ffc1ff4ae25
archer <py@ualberta.ca>
2023-01-19 19:35:26 -0700

    backend and mongodb connection

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,5 +1,10 @@
 import express from 'express';
 
+import { getPosts, createPosts } from '../controllers/posts.js';
+
 const router = express.Router();
 
-router.get('/')
+router.get('/', getPosts);
+router.post('/', createPosts);
+
+export default router;
